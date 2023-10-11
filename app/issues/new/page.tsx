3bd73c7b67231d4d49814fefb78b04ea1ac44911
IveeDev <iviidev899@gmail.com>
2023-10-11 13:07:59 +0100

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,10 @@
 import dynamic from "next/dynamic";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
 // import IssueForm from "../_components/IssueForm";
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
-  loading: () => <p>Loading...</p>,
+  loading: () => <IssueFormSkeleton />,
 });
 
 const NewIssuePage = () => {
