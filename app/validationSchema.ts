eebae3ad0fa6051d2423592f4db0a73e5f4a5a62
IveeDev <iviidev899@gmail.com>
2023-10-31 17:17:10 +0100

    Fixed validation schema

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -5,7 +5,6 @@ export const IssueSchema = z.object({
   description: z.string().min(1, "description is required.").max(65535),
 });
 
-
 export const PatchIssueSchema = z.object({
   title: z.string().min(1, "title is required.").max(255).optional(),
   description: z
